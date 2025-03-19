f7bb314f34a4aa89e53c53443cf7d52f4bfb70ae
Dantas <marcelomurilotrabalho@gmai.com>
2025-03-19 14:07:30 -0300

    empresasReservasRoutes

--- volley-backend/src/routes/empresasReservasRoutes.js
+++ volley-backend/src/routes/empresasReservasRoutes.js
@@ -1,5 +1,3 @@
-// src/routes/empresasReservasRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const db = require('../db');
@@ -9,7 +7,7 @@ const authMiddleware = require('../middlewares/authMiddleware');
 router.use(authMiddleware);
 
 /**
- * GET /api/empresas/:id_empresa/reservas
+ * GET /api/empresas/reservas/:id_empresa/reservas
  * Query: status (opcional, por exemplo: pendente)
  * Retorna as reservas da empresa, filtrando pelo status se fornecido.
  */
@@ -18,15 +16,28 @@ router.get('/:id_empresa/reservas', async (req, res) => {
   const { status } = req.query; // ex: status=pendente
 
   try {
+    console.log('Executando consulta SQL: ', `
+      SELECT r.id_reserva, r.id_jogo, r.data_reserva, r.horario_inicio, r.horario_fim, r.status,
+             r.descricao, j.id_usuario, u.nome AS organizador, j.limite_jogadores as quantidade_jogadores
+        FROM reservas r
+        JOIN jogos j ON r.id_jogo = j.id_jogo
+        JOIN usuario u ON j.id_usuario = u.id_usuario
+       WHERE r.id_empresa = $1
+       ${status ? 'AND r.status = $2' : ''}
+       ORDER BY r.data_reserva, r.horario_inicio
+     | Parâmetros: ${JSON.stringify(status ? [id_empresa, status] : [id_empresa])}`);
+    
     const query = `
       SELECT r.id_reserva, r.id_jogo, r.data_reserva, r.horario_inicio, r.horario_fim, r.status,
-             r.descricao, u.nome AS organizador, r.quantidade_jogadores
+             r.descricao, j.id_usuario, u.nome AS organizador, j.limite_jogadores as quantidade_jogadores
         FROM reservas r
-        JOIN usuario u ON r.id_usuario = u.id_usuario
+        JOIN jogos j ON r.id_jogo = j.id_jogo
+        JOIN usuario u ON j.id_usuario = u.id_usuario
        WHERE r.id_empresa = $1
        ${status ? 'AND r.status = $2' : ''}
        ORDER BY r.data_reserva, r.horario_inicio
     `;
+    
     const params = status ? [id_empresa, status] : [id_empresa];
     const result = await db.query(query, params);
     return res.json(result.rows);
