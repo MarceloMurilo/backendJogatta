bd28f039baf2178b0fae56b6538e0240ad92344d
Dantas <marcelomurilotrabalho@gmai.com>
2025-03-19 13:33:20 -0300

    empresasReservasRoutes

--- volley-backend/src/routes/empresasReservasRoutes.js
+++ volley-backend/src/routes/empresasReservasRoutes.js
@@ -22,7 +22,8 @@ router.get('/:id_empresa/reservas', async (req, res) => {
       SELECT r.id_reserva, r.id_jogo, r.data_reserva, r.horario_inicio, r.horario_fim, r.status,
              r.descricao, u.nome AS organizador, r.quantidade_jogadores
         FROM reservas r
-        JOIN usuario u ON r.id_usuario = u.id_usuario
+        JOIN usuario u ON r.id_reserva_usuario = u.id_usuario
+
        WHERE r.id_empresa = $1
        ${status ? 'AND r.status = $2' : ''}
        ORDER BY r.data_reserva, r.horario_inicio
