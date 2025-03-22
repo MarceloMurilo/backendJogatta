4e8b22df3792fbb154d5333bacf7af00e0523cde
Dantas <marcelomurilotrabalho@gmai.com>
2025-03-22 18:26:07 -0300

    esqueci de comittar isso aafff

--- volley-backend/src/server.js
+++ volley-backend/src/server.js
@@ -73,7 +73,7 @@ app.use('/api/connect', stripeConnectRoutes);
 // Importação de rotas
 // ------------------------------------------------
 const jogadorRoutes = require('./routes/jogador/jogadorRoutes');
-const reservationRoutes = require('./routes/jogador/reservationRoutes');
+const reservationRoutes = require('./routes/jogador/reservationRoutes.js');
 const jogosRoutes = require('./routes/jogador/jogosRoutes');
 const courtManagementRoutes = require('./routes/owner/courtManagementRoutes');
 const ownerReservationsRoutes = require('./routes/owner/ownerReservationsRoutes');
